6293eac16407707267f267413dfd3308bbfa97ea
CodeCool2006 <rchavadar@gmail.com>
2026-03-23 10:56:03 +0530

    first commit

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+// var a = 10;
+// let b = 20;
+// const c = 30;
+// console.log(a, b, c);
+
+let name = "R";
+let age = 24;
+let isStudent = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+
