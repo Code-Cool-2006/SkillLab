fde87368868b2d4ca03fa0efd44deb521d33bee9
CodeCool2006 <rchavadar@gmail.com>
2026-03-23 11:40:10 +0530

    JavaScript ES6 Functions

--- Javascript.js
+++ Javascript.js
@@ -3,11 +3,58 @@
 // const c = 30;
 // console.log(a, b, c);
 
-let name = "R";
-let age = 24;
-let isStudent = true;
+// let name = "R";
+// let age = 24;
+// let isStudent = true;
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isStudent);
 
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof isStudent);
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(2, 3));
 
+// let fruits = ["apple", "banana", "orange", "mango"];
+// console.log(fruits[0]);
+// fruits.push("papaya");
+// console.log(fruits);
+
+// let person = {
+//   name: "Rishab",
+//   age: 24,
+// };
+// console.log(person.name);
+// console.log(person.age);
+
+// ES6 Functions
+
+// Arrow Function
+// const add = (a, b) => a + b;
+// console.log(add(2, 3));
+
+//TemplateLiterals
+// let name = "Rishab";
+// let age = 20;
+// console.log(`My name is ${name} and I am ${age} years old`);
+
+//SpreadOperator
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+// let arr3 = [...arr1, ...arr2];
+// console.log(arr3);
+
+// Map Function
+// let nums = [1, 2, 3, 4, 5];
+// let double = nums.map((num) => num * 2);
+// console.log(double);
+
+// Filter Function
+// let nums = [1, 2, 3, 4, 5];
+// let x = nums.filter((num) => num % 2 === 0);
+// console.log(x);
+
+// Find Function
+// let nums = [10, 20, 30];
+// let value = nums.find((num) => num > 15);
+// console.log(value);
